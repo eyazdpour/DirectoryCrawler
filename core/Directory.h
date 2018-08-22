2a32fe75e9c900fe3fc09304762b525545fb2d15
yazdpour <yazdpour@gmail.com>
2018-08-23 00:52:15 +0430

    Directory Class has been added with an inside crawler

--- core/Directory.h
+++ core/Directory.h
@@ -1,31 +1,104 @@
-#ifndef FOLDERS_H
-#define FOLDERS_H
+#ifndef DIRECTORY_H
+#define DIRECTORY_H
 
+#include<vector>
 #include "./dataStructure.h"
+
 using namespace std;
 
 class Directory
 {
-    public:
-        //CONSTRUCTOR
-        Directory(int directoryId,string path,vector<File> files, vector<Directory> directories);
-        
-        //GETTERS
-        int get_directoryId();
-        string get_path();
-        vector<File> get_files();
-        vector<Directory> get_directories();
+  public:
+    //CONSTRUCTOR
+    Directory(int directoryId, string path);
 
-        //METHODS
+    //GETTERS
+    int get_directoryId();
+    string get_path();
+    string get_name();
+    vector<File> get_files();
+    vector<Directory> get_directories();
 
-    
-    private:
-        int directoryId;
-        string path;
-        vector<File> files;
-        vector<Directory> directories;
+    //METHODS
+    void save(); //to file in json format or xml
+    void load(); //from file in json format or xml
+    void crawl_directories();
+    void crawl_files();
+    bool has_directories();
+    bool has_files();
+    string list_directories();
+    string list_files();
+
+  private:
+    int directoryId;
+    string path;
+    string name;
+    vector<File> files;
+    vector<Directory> directories;
 };
 
 // Member functions definitions including constructor
+//CONSTRUCTOR:
+Directory::Directory(int directoryId, string path)
+{
+    this->directoryId = directoryId;
+    this->path = path;
+}
+
+//GETTERS:
+int Directory::get_directoryId(){
+    return this->directoryId;
+}
+string Directory::get_path(){
+    return this->path;
+}
+string Directory::get_name(){
+    return this->name;
+}
+vector<File> Directory::get_files(){
+    return this->files;
+}
+vector<Directory> Directory::get_directories(){
+    return this->directories;
+}
+
+//METHODS:
+void Directory::save(){
+
+}
+void Directory::load(){
+
+}
+void Directory::crawl_directories(){
+    DIR* dirp = opendir(this->path.c_str());
+    struct dirent * dp;
+    int id = 0;
+    while ((dp = readdir(dirp)) != NULL) {
+         if(dp->d_type==16) /*for windows only*/ {
+           this->directories.push_back(Directory(1,dp->d_name));
+           id++;
+         }
+         
+    }
+    closedir(dirp);
+}
+void Directory::crawl_files(){
+
+}
+bool Directory::has_directories(){
+    return (this->directories.size()>0)?true:false;
+}
+bool Directory::has_files(){
+    return (this->files.size()>0)?true:false;
+}
+string Directory::list_directories(){
+    string list = "Path: "+this->path+"\n";
+    for(int i =0;i<this->directories.size();i++)
+        list+="\t"+this->directories[i].path+"\n";
+    return list;
+}
+string Directory::list_files(){
+    return "";
+}
 
 #endif
--- sample/main.cpp
+++ sample/main.cpp
@@ -4,18 +4,18 @@
 
 #include "../core/crawler.h"
 #include "../core/dataStructure.h"
+#include "../core/Directory.h"
 
 int main()
 {
     
-    Folders f1 = Crawler::read_directory(".");
-    vector<string> dirNames = f1.get_folders();
-    for(int i =0 ; i<dirNames.size() ;i++){
-        cout<<dirNames[i]<<endl;
-    }
+    // Folders f1 = Crawler::read_directory(".");
+    // vector<string> dirNames = f1.get_folders();
+    // for(int i =0 ; i<dirNames.size() ;i++){
+    //     cout<<dirNames[i]<<endl;
+    // }
+
+    Directory* d1 = new Directory(0,".");
+    d1->crawl_directories();
+    std::cout<<(d1->list_directories())<<std::endl;
 }
-/************************
- * dataStructure.h:
- *   Folder class is something more than enough and should be removed ASAP.
- *   We should only have a Folders Class including of a vector<string> prop.
- ***********************/
