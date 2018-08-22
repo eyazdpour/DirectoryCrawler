c39a67d2414cb879abbaf092e13f658b59be8abd
yazdpour <e.yazdpour@gmail.com>
2018-08-23 03:07:55 +0430

    get_name() example has been added

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 #include "../core/Directory.h"
 
 int main()
 {
-    Directory* d1 = new Directory("C:/");
+    Directory *d1 = new Directory("C:/Erfan\\");
     d1->crawl();
-    std::cout<<"----------Directories----------\n"<<(d1->list_directories())<<std::endl;
-    std::cout<<"----------Files----------\n"<<(d1->list_files())<<std::endl;
+    //cout<<d1->get_name()<<endl;
+    std::cout << "----------Directories----------\n"
+              << (d1->list_directories()) << std::endl;
+    std::cout << "----------Files----------\n"
+              << (d1->list_files()) << std::endl;
 }
